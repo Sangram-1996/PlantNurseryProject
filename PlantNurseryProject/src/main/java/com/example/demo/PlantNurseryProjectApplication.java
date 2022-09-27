0d39f3f610719bb8b663a9f2659b2edb94b6d249
Sangram-1996 <sangramkesharisahu96@gmail.com>
2022-09-27 16:22:33 +0530

    project init

---
+++ PlantNurseryProject/src/main/java/com/example/demo/PlantNurseryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantNurseryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantNurseryProjectApplication.class, args);
+	}
+
+}
